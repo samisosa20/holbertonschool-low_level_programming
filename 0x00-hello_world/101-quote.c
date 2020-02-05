3626286d057ffc7bdb0eebdd4772a2de7eb14be9
samisosa20 <1449@holbertonschool.com>
2020-02-05 21:10:27 +0000

    0x00-hello-world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
