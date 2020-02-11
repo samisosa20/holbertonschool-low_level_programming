c11cd652cc8e4354d6406c2fbf6c3b13b3ddea15
samisosa20 <1449@holbertonschool.com>
2020-02-11 12:19:50 +0000

    fix_code

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,6 +11,7 @@ int print_last_digit(int i)
 
 	if (i < 0)
 		i = i * -1;
+
 	j = i % 10;
 	_putchar(j + '0');
 	return (j);
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,24 +17,22 @@ void times_table(void)
 			c = a * b;
 			if (c < 10)
 			{
-				_putchar(c  + '0');
-				if (b < 9)
+				if (b != 0)
 				{
-					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 				}
+				_putchar(c + '0');
 			} else
 			{
+				_putchar(' ');
 				_putchar(c / 10 + '0');
 				_putchar(c % 10 + '0');
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
 			}
+			if (b != 9)
+				_putchar(',');
+			else
+				_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
