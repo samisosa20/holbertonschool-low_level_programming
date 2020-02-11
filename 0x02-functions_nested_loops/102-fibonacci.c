d9c3b28db3e6ea7e83e2f3ac42cbed43f2b5e12d
samisosa20 <1449@holbertonschool.com>
2020-02-11 23:48:55 +0000

    change_variable_long

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,20 +7,20 @@
 */
 int main(void)
 {
-	int i, a, b, res;
+	long i, a, b, res;
 
 	res = 0;
 	a = 1;
 	b = 2;
-	for (i = 1; i <= 50; i++)
+	for (i = 0; i < 50; i++)
 	{
 		res = a + b;
 		a = b;
 		b = res;
-		if ( i != 50)
-			printf("%d, ", res);
+		if (i != 49)
+			printf("%ld, ", res);
 		else
-			printf("%d\n", res);
+			printf("%ld\n", res);
 	}
 	return (0);
 }
