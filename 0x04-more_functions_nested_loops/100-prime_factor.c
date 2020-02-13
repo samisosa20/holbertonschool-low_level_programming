d538e540131a574ee9d1e64fd42a1fb400ade125
samisosa20 <1449@holbertonschool.com>
2020-02-13 19:32:01 +0000

    add_sqrt

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 /**
 * main -  finds and prints the largest
 * prime factor of the number 612852475143.
@@ -8,19 +9,17 @@
 
 int main(void)
 {
-	long number, d, last_number, aux;
+	long number, d, aux;
 
 	number = aux = 612852475143;
-	for (d = 2; d < number; d++)
+	for (d = 2; d < sqrt(number); d++)
 	{
 		if (aux % d == 0)
 		{
-			last_number = d;
 			number = number / d;
-			d = 2;
-			/*printf("%ld - %ld, ", number, d);*/
+			d = 1;
 		}
 	}
-	printf("%ld\n", last_number);
+	printf("%ld\n", number);
 	return (0);
 }
