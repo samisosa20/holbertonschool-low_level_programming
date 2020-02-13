adc1328940b1e4b6102d843e0028014ba3f2fccc
samisosa20 <1449@holbertonschool.com>
2020-02-13 21:02:14 +0000

    fix_INT_MIN

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <limits.h>
 
 /**
 * print_number - print any number.
@@ -7,6 +8,14 @@
 */
 void print_number(int n)
 {
+	int aux = 0;
+
+	if ( n == INT_MIN)
+	{
+		n = n + 1;
+		aux = 1;
+	}
+
 	if (n < 0)
 	{
 		n = -n;
@@ -15,5 +24,12 @@ void print_number(int n)
 
 	if (n / 10 != 0)
 		print_number(n / 10);
-	_putchar(n % 10 + '0');
+
+	if (aux == 1)
+	{
+		 n = 8;
+		_putchar(n % 10 + '0');
+	}
+	else
+		_putchar(n % 10 + '0');
 }
--- 0x04-more_functions_nested_loops/main/101-main.c
+++ 0x04-more_functions_nested_loops/main/101-main.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <limits.h>
 /**
  * main - check the code for Holberton School students.
  *
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-  print_number(98);
+  print_number(-4096);
   _putchar('\n');
-  print_number(402);
+  print_number(123456789);
   _putchar('\n');
-  print_number(1024);
+  print_number(INT_MAX);
   _putchar('\n');
-  print_number(0);
+  print_number(INT_MIN);
   _putchar('\n');
-  print_number(-98);
+  print_number(-99);
   _putchar('\n');
   return (0);
 }
