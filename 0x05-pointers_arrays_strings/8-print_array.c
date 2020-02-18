30e13feb3204a314cfa843103b8aff15d42b35d2
samisosa20 <1449@holbertonschool.com>
2020-02-18 15:39:42 +0000

    Change_if

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,14 +10,13 @@
 */
 void print_array(int *a, int n)
 {
-	int b = 0;
+	int b;
 
-	for (;b < n; b++)
+	for (b = 0; b < n; b++)
 	{
-		if (b == 0)
-			printf("%d", a[b]);
-		else
-			printf(" ,%d", a[b]);
+		printf("%d", a[b]);
+		if (b != (n - 1))
+			printf(", ");
 	}
 	printf("\n");
 }
