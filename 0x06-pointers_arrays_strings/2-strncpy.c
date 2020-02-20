b4fd1d5705cc882ac4ad339e8ed4dbf46e9e70c0
samisosa20 <1449@holbertonschool.com>
2020-02-20 17:51:05 +0000

    fix-code

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,12 +10,11 @@
 char *_strncpy(char *dest, char *src, int n)
 
 {
-	size_t i;
-	size_t m = n;
+	int i;
 
-	for (i = 0; i < m && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	for ( ; i < m; i++)
+	for ( ; i < n; i++)
 		dest[i] = '\0';
 
 	return (dest);
