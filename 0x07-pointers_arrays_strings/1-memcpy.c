7fa2259e5f50b7fd167ae35e503f7fb119d14843
samisosa20 <1449@holbertonschool.com>
2020-02-24 17:38:39 +0000

    fix-code

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,16 +8,10 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int dest_len = 0;
 	unsigned int i;
 
-	while (dest[dest_len] != '\0')
-	{
-		dest_len++;
-	}
-
-	for (i = 0 ; i < n && src[i] != '\0'; i++)
-		dest[dest_len + i] = src[i];
+	for (i = 0 ; i < n; i++)
+		dest[i] = src[i];
 
 	return (dest);
 }
