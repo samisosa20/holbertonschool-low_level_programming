e02fb7922e2a5d21d71be2c4b9d160e0acae64a1
samisosa20 <1449@holbertonschool.com>
2020-03-03 18:03:32 +0000

    fix-code-restriction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,13 +11,21 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j;
 	char *a;
 
-	for (i = 0; s1[i]; i++)
-		;
-	for (j = 0; s2[j]; j++)
-		;
-	i++;
-	j++;
-	a = malloc((j + i) * sizeof(char));
+	if (s1 == NULL)
+		i = 0;
+	else
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
+	if (s2 == NULL)
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	a = malloc((j + i + 1) * sizeof(char));
 	if (a == NULL)
 		return (NULL);
 	for (i = 0; s1[i]; i++)
