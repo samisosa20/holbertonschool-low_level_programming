ce9335cffaf145305568c5b60c3bc6990e5da170
samisosa20 <1449@holbertonschool.com>
2020-03-05 18:45:49 +0000

    fix-code

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,7 +8,7 @@
   */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
+	/*signed int i;*/
 	void *a;
 
 	if (size == 0 || nmemb == 0)
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	a = malloc(size * nmemb);
 	if (a == NULL)
 		return (NULL);
-	for (i = 0; i < (size * nmemb); i++)
-		a[i] = '\0';
+	/*for (i = 0; i < (size * nmemb); i++)
+		a[i] = '\0';*/
 	return (a);
 }
