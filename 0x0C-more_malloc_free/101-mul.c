54bd6fce89ae1d60aa10689acde5e8147af3103d
samisosa20 <1449@holbertonschool.com>
2020-03-06 00:26:45 +0000

    0x0C-more_malloc

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - a program that multiplies two numbers.
+  * @argc: count of the arguments supplied to the program and the
+  * @argv: an array of pointers to the strings
+  * Return: always 0.
+  */
+int main(int argc, char *argv[])
+{
+	int count, multi = 1;
+
+	if (argc > 2)
+	{
+		for (count = 1; count < argc; count++)
+		{
+			if(atoi(argv[count]) >= 100 || atoi(argv[count]) < 0)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+			multi = multi * atoi(argv[count]);
+		}
+		printf("%d\n", multi);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (98);
+	}
+}
