a5e674ff4b3fbb0eb27782b24192b5f1adb1c8c4
Sammy <1449@holbertonschool.com>
2020-03-11 19:38:29 +0000

    fix-code

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -15,11 +15,11 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
 	while (i < 5)
 	{
-		if (*(ops[i].op) == *s)
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,6 +11,11 @@ int main(int argc, char *argv[])
 {
 	if (argc == 4)
 	{
+		if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+		{
+			printf("Error\n");
+			exit(100);
+		}
 		printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	}
 	else
