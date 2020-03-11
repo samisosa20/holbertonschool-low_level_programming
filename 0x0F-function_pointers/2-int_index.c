b38b9f358b6a031ebe09cb2ba193c3d72eaeb2ae
Sammy <1449@holbertonschool.com>
2020-03-11 18:23:14 +0000

    fix-code

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,10 +10,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-	for (i = 0; i < size; i++)
-		if ((cmp)(array[i]) == 1)
-			return (i);
+	if (cmp && array)
+	{
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+			if ((cmp)(array[i]) == 1)
+				return (i);
+	}
 	return (-1);
 }
