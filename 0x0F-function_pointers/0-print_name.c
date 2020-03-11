fd99383a2f3a3957838d3547b2dac88e12ab71ca
Sammy <1449@holbertonschool.com>
2020-03-11 18:09:26 +0000

    0x0F-function_pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - check the code for Holberton School students.
+ * @name: name to print UPPER.
+ * @f: function pointer.
+ * Return: none.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(f)(name);
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * int_index - function that searches for an integer
+ * @array: name to print UPPER.
+ * @size: size of array.
+ * @cmp: function pointer.
+ * Return: Always 0.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if ((cmp)(array[i]) == 1)
+			return (i);
+	return (-1);
+}
