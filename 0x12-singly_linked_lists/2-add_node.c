31cafb57df8c518cfea50b060c091fe4f72ed7a5
Sammy <1449@holbertonschool.com>
2020-03-19 17:44:43 +0000

    test check

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,7 +18,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->len = strlen(str);
+	/*new->len = strlen(str);*/
 	new->next = *head;
 	*head = new;
 	return (new);
