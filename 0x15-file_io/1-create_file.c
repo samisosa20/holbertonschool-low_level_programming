923af31ddd860db38763cb7a4e0a8d62e90f2c4a
Sammy <1449@holbertonschool.com>
2020-03-31 18:27:38 +0000

    add condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,8 +32,11 @@ int create_file(const char *filename, char *text_content)
 	of = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (of == -1)
 		return (0);
-	leng = _strlen(text_content);
-	wf = write(of, text_content, leng);
+	if (text_content != NULL)
+	{
+		leng = _strlen(text_content);
+		wf = write(of, text_content, leng);
+	}
 	if (wf == -1)
 		return (0);
 	cf = close(of);
