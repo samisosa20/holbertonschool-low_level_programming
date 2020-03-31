d95642df2651cc2b8ffe1baf4e5f412b09e5fc31
Sammy <1449@holbertonschool.com>
2020-03-31 18:36:47 +0000

    fix wrong return

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -32,15 +32,15 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	of = open(filename, O_APPEND | O_RDWR);
 	if (of == -1)
-		return (0);
+		return (-1);
 	if (text_content == NULL)
 		return (1);
 	leng = _strlen(text_content);
 	wf = write(of, text_content, leng);
 	if (wf == -1)
-		return (0);
+		return (-1);
 	cf = close(of);
 	if (cf == -1)
-		return (0);
+		return (-1);
 	return (1);
 }
