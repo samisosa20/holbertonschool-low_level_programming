d760b66fea4828600b6494837e2302822bbdbe60
Sammy <1449@holbertonschool.com>
2020-03-31 18:24:47 +0000

    Fix counter

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,26 +9,29 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int of = 0, cf = 0, rf = 0, wf = 0;
-	char *buf;
+	int of = 0, cf = 0, rf = 0;
+	unsigned int i;
+	char buf;
 
 	if (filename == NULL)
 		return (0);
 	of = open(filename, O_RDWR);
 	if (of == -1)
 		return (0);
-	buf = malloc(letters + 1);
-	if (buf == NULL)
-		return (0);
-	rf = read(of, buf, letters);
+	rf = read(of, &buf, 1);
 	if (rf == -1)
 		return (0);
-	wf = write(STDOUT_FILENO, buf, letters);
-		if (wf == -1)
+	for (i = 0; rf != 0 && i != letters; i++)
+	{
+		rf = write(STDOUT_FILENO, &buf, 1);
+		if (rf == -1)
 			return (0);
+		rf = read(of, &buf, 1);
+		if (rf == -1)
+			return (0);
+	}
 	cf = close(of);
 		if (cf == -1)
 			return (0);
-	return (letters);
-	free(buf);
+	return (i);
 }
