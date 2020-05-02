25f32a64caf2233b9c59c55f9f7526bde38d6f20
samisosa20 <1449@holbertonschool.com>
2020-05-02 16:44:41 +0000

    0x17-doubly

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,11 +1,50 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index -
- *
- * Return: 
+ * insert_dnodeint_at_index - function that inserts a
+ * new node at a given position.
+ * @h: address list
+ * @idx: index
+ * @n: value to insert
+ * Return: list
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *current = *h;
+	unsigned int iterate = 0;
+	dlistint_t *new = malloc(sizeof(dlistint_t));
 
+	if (new == NULL || h == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	/* if index is head, then, set new node as head */
+	if (idx == 0)
+	{
+		new = add_dnodeint(h, n);
+		return (new);
+	}
+	/* if next node is not NULL & points to the node before insertion */
+	/* if pointer is at the node before insertion */
+	while ((iterate <= (idx - 1)) && (current != NULL))
+	{
+		if ((current->next == NULL) && (iterate == (idx - 1)))
+		{
+			new = add_dnodeint_end(h, n);
+			return (new);
+		}
+		if (iterate == (idx - 1))
+		{
+			new->prev = current;
+			new->next = current->next;
+			current->next->prev = new;
+			current->next = new;
+			return (new);
+		}
+		current = current->next;
+		iterate++;
+	}
+	free(new);
+	return (NULL);
 }
