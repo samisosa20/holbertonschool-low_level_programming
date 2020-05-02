664e43972bd0a35ef301d19e3c9a51b323b2c5f8
samisosa20 <1449@holbertonschool.com>
2020-05-02 16:19:51 +0000

    0x17-doubly

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -7,5 +7,17 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	unsigned int iterate = 0;
 
+	while (head != NULL)
+	{
+		if (iterate == index)
+		{
+			return (head);
+		}
+		iterate++;
+		head = head->next;
+	}
+	/* all else, return NULL */
+	return (NULL);
 }
--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -7,5 +7,16 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
+	int sum = 0;
 
+	if (head)
+	{
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+		return (sum);
+	}
+	return (0);
 }
