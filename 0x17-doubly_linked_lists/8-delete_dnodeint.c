d5f55d6f695dabcd0abd5594af6c5fe1ac5efb8b
samisosa20 <1449@holbertonschool.com>
2020-05-03 13:49:19 +0000

    add conditional

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,31 +9,31 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int i = 0;
-	dlistint_t *aux = *head;
-	dlistint_t *new;
+	dlistint_t *current;
+	unsigned int i;
 
-	if (!aux)
+	if (head == NULL || *head == NULL)
 		return (-1);
-	while (*head && index)
+	current = *head;
+	if (index == 0)
 	{
-		if (!aux)
+		*head = current->next;
+		if (current->next != NULL)
+		{
+			current->next->prev = NULL;
+		}
+		free(current);
+		return (1);
+	}
+	for (i = 0; i < index; i++)
+	{
+		if (current->next == NULL)
 			return (-1);
-		if (i == index - 1)
-			break;
-		aux = aux->next;
-		++i;
+		current = current->next;
 	}
-	if (index)
-	{
-		new = aux->next->next;
-		free(aux->next);
-		aux->next = new;
-	}
-	else
-	{
-		*head = (*head)->next;
-		free(aux);
-	}
+	current->prev->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+	free(current);
 	return (1);
 }
