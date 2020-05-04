8362938b4472dc477940b4c54409e0ec7e8d2562
samisosa20 <1449@holbertonschool.com>
2020-05-04 18:33:41 +0000

    change name directory

---
+++ 0x18-dynamic_libraries/files/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+/**
+* _strpbrk - a function that searches
+* a string for any of a set of bytes.
+* @s: pointer that locate a character.
+* @accept: pointer that content character.
+* Return: pointer.
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j = 0, t = 0;
+
+	while (s[i] != '\0')
+	{
+		t = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				t = 1;
+		}
+		if (t == 1)
+			return (s + i);
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x18-dynamic_libraries/files/9-strcpy.c
@@ -0,0 +1,20 @@
+/**
+* *_strcpy - function that copies the string pointed to by src,
+* including the terminating null byte (\0), to the buffer pointed to by dest.
+* @dest: pointer to paste
+* @src: pointer to copy
+* Return: pointer.
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int x = 0;
+
+	while (src[x] != '\0')
+	{
+		dest[x] = src[x];
+		x++;
+	}
+
+	dest[x] = '\0';
+	return (dest);
+}
