1a5f6aa05215dfecff60221332a873377b56d860
samisosa20 <1449@holbertonschool.com>
2020-05-29 14:27:46 +0000

    add free when remplace

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,6 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
+			free(ht->array[i]->value);
 			ht->array[i]->value = (char *)value;
 			return (1);
 		}
