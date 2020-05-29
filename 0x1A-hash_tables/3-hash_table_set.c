3c55e417ece6416314d7857d3b6cc89d2198e450
samisosa20 <1449@holbertonschool.com>
2020-05-29 14:33:09 +0000

    add malloc to save

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,18 +10,22 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
+	char *value_copy;
 	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
-			ht->array[i]->value = (char *)value;
+			ht->array[i]->value = value_copy;
 			return (1);
 		}
 	}
@@ -29,8 +33,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
-	new->key = (char *)key;
-	new->value = (char *)value;
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
 	new->next = ht->array[index];
 	ht->array[index] = new;
 
